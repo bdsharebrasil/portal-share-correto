58d7362547e089ee3f52ffbfd9752d053ec39a14
Sharebrasil <sharebrasilmt@gmail.com>
2026-01-17 21:32:37 -0300

    Delete src directory

--- src/components/consolidation/index.ts
+++
@@ -1,4 +0,0 @@
-export { HistoricoRateioConsolidado } from './HistoricoRateioConsolidado';
-export { HorasMensaisConsolidadas } from './HorasMensaisConsolidadas';
-export { RelatorioCustosCliente } from './RelatorioCustosCliente';
-export { BalancoClienteAeronave } from './BalancoClienteAeronave';
--- src/components/plano-voo/index.ts
+++
@@ -1,4 +0,0 @@
-// Flight Plan Components - Clean Architecture
-export { FlightPlanMap, type Waypoint, type LegInfo } from './FlightPlanMap';
-export { FlightRouteMap, type RoutePoint } from './FlightRouteMap';
-export { Step1AircraftInfo } from './Step1AircraftInfo';
