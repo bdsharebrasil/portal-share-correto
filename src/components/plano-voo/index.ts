9bd7d1f61564aa716f57e40d2ff8339fa2f34501
bdsharebrasil <bdsharebrasil@gmail.com>
2026-02-10 11:45:42 -0300

    Delete src/components/plano-voo directory

--- src/components/plano-voo/index.ts
+++
@@ -1,5 +0,0 @@
-// Flight Plan Components - Clean Architecture
-export { FlightRouteMap, type RoutePoint } from './FlightRouteMap';
-export { Step1AircraftInfo } from './Step1AircraftInfo';
-export { AeronauticalInfoTab } from './AeronauticalInfoTab';
-export { RouteStatusBadge, NOTAMSummary, NavaidsDisplay } from './RouteValidationComponents';
