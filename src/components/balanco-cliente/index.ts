cc43ed2f00c831d69a780662dba6013a5170ab1d
bdsharebrasil <bdsharebrasil@gmail.com>
2026-02-25 11:15:44 -0300

    Delete src directory

--- src/components/balanco-cliente/index.ts
+++
@@ -1,5 +0,0 @@
-export { BalancoVisaoGeral } from './BalancoVisaoGeral';
-export { DespesasDetalhadas } from './DespesasDetalhadas';
-export { PendenciasFinanceiras } from './PendenciasFinanceiras';
-export { BalancoAeronave } from './BalancoAeronave';
-export { RelatoriosExportacao } from './RelatoriosExportacao';
