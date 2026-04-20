d6a700bd6e1bcec8db713362f83979f94556798a
testeunkportal <testeunkportal@gmail.com>
2026-04-20 14:32:18 +0000

    up

--- src/components/diario/DiarioBordo/hooks/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Central exports for Diario de Bordo hooks
- */
-
-// Hooks
-export { useDiarioData } from './useDiarioData';
-export { useFlightCalculations } from './useFlightCalculations';
-export { usePerDiem } from './usePerDiem';
-export { useCelulaManagement } from './useCelulaManagement';
