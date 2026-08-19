b884f45a51d76dc9953096a84d808f7d895c31e8
bdsharebrasil <bdsharebrasil@gmail.com>
2026-08-19 06:14:08 -0300

    Excluir aba de férias do funcionário (#113)

--- src/components/RelatorioDespesaViagem/index.ts
+++
@@ -1,8 +0,0 @@
-export { TravelReportForm } from './TravelReportForm';
-export { PastasRelatorios } from './PastasRelatorios';
-export { ReceiptPreviewModal } from './ReceiptPreviewModal';
-export { SearchInput } from './SearchInput';
-export { TravelReportStatusManager, type TravelReportStatus } from './TravelReportStatusManager';
-export { RelatorioViagemViewer } from './RelatorioViagemViewer';
-export { AircraftFolderButton } from './AircraftFolderButton';
-export { TravelReportsFolder } from './TravelReportsFolder';
