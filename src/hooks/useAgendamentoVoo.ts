bc547d6381e57d11a3084d4e3763d5a54d09e804
bdsharebrasil <bdsharebrasil@gmail.com>
2026-08-19 10:04:17 -0300

    Update useAgendamentoVoo.ts

--- src/hooks/useAgendamentoVoo.ts
+++ src/hooks/useAgendamentoVoo.ts
@@ -1020,7 +1020,6 @@ export function useAgendamentoMutations() {
           solicitacao.numero_voo ??
           null,
 
-        jornada_id: null,
       };
 
       const { data, error: insertError } =
