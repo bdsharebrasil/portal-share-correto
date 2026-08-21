6885b8275cbd9f31f539c2832caa61a78da0cba3
bdsharebrasil <bdsharebrasil@gmail.com>
2026-08-21 10:44:09 -0300

    feat(theme): carregar overrides visuais depois do design system

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './theme-overrides.css'
 import './lib/fetch-retry' // Initialize fetch retry interceptor
 
 createRoot(document.getElementById("root")!).render(<App />);
