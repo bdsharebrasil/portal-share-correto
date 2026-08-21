bdc1aff74dd53b05682133a7cee3f49a7af55e2c
bdsharebrasil <bdsharebrasil@gmail.com>
2026-08-21 18:50:23 -0300

    feat(plano-voo): carregar estilos responsivos para mobile e iPad

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './plano-voo-mobile.css'
 import './lib/fetch-retry' // Initialize fetch retry interceptor
 
 createRoot(document.getElementById("root")!).render(<App />);
