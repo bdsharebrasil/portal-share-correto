089ef4c0e7c653d709d545616844cee15663c0a3
bdsharebrasil <bdsharebrasil@gmail.com>
2026-08-21 12:18:02 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import './theme-overrides.css'
 import './lib/fetch-retry' // Initialize fetch retry interceptor
 
 createRoot(document.getElementById("root")!).render(<App />);
